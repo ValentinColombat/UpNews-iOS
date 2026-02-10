c63817453fe76e2e64aeabe048aeb7256834451c
Valentin <valentincolombat@gmail.com>
2026-02-10 15:51:57 +0100

    fix : standart widget

--- UpNewsWidget/UpNewsWidget.swift
+++ UpNewsWidget/UpNewsWidget.swift
@@ -1,94 +1,47 @@
-//
-//  UpNewsWidget.swift
-//  UpNewsWidget
-//
-//  Widget simplifié pour test
-
 import WidgetKit
 import SwiftUI
 
-// MARK: - Timeline Provider
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), message: "Hello World")
+        SimpleEntry(date: Date())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), message: "Hello World")
-        completion(entry)
+        completion(SimpleEntry(date: Date()))
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        let entry = SimpleEntry(date: Date(), message: "Hello World")
-        let timeline = Timeline(entries: [entry], policy: .atEnd)
+        let timeline = Timeline(entries: [SimpleEntry(date: Date())], policy: .atEnd)
         completion(timeline)
     }
 }
 
-// MARK: - Widget Entry
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let message: String
-}
-
-// MARK: - Widget View
-
-struct UpNewsWidgetEntryView : View {
+struct UpNewsWidgetEntryView: View {
     var entry: Provider.Entry
     
     var body: some View {
-        VStack {
-            Text("👋")
-                .font(.system(size: 50))
-            
-            Text(entry.message)
-                .font(.title)
-                .fontWeight(.bold)
-            
-            Text(entry.date, style: .time)
-                .font(.caption)
-                .foregroundColor(.secondary)
-        }
-        .containerBackground(for: .widget) {
-            Color.orange.opacity(0.2)
-        }
+        Text("Hello")
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .bold()
+            .containerBackground(for: .widget) {
+                Color.blue
+            }
     }
 }
-
-// MARK: - Widget Configuration
 
 @main
 struct UpNewsWidget: Widget {
-    let kind: String = "UpNewsWidget"
-
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        StaticConfiguration(kind: "UpNewsWidget", provider: Provider()) { entry in
             UpNewsWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("Hello World Widget")
-        .description("Widget de test simplifié")
-        .supportedFamilies([.systemSmall, .systemMedium])
-    }
-}
-
-// MARK: - Preview
-
-struct UpNewsWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            UpNewsWidgetEntryView(entry: SimpleEntry(
-                date: Date(),
-                message: "Hello World"
-            ))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-            
-            UpNewsWidgetEntryView(entry: SimpleEntry(
-                date: Date(),
-                message: "Hello World"
-            ))
-            .previewContext(WidgetPreviewContext(family: .systemMedium))
-        }
+        .configurationDisplayName("Hello Widget")
+        .description("Widget basique")
+        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
     }
 }
